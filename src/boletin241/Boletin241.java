416d4534ab59f71d9d13e51c68661ead1b24375f
sbenavidesvazquez <sbenavidesvazquez@atanasoft15.danielcastelao.org>
2015-04-15 10:34:45 +0200

    boletin 24

---
+++ src/boletin241/Boletin241.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package boletin241;
+
+/**
+ *
+ * @author sbenavidesvazquez
+ */
+public class Boletin241 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
